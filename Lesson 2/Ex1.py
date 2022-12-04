e357780dde7106a40123bd0add1337a08b14c678
V1rru <97950906+V1rru@users.noreply.github.com>
2022-12-04 14:35:03 +0300

    Create Ex1.py

---
+++ Lesson 2/Ex1.py
@@ -0,0 +1,20 @@
+firstNumber = float(input("Enter first number: "))
+secondNumber = float(input("Enter second number: "))
+if float(firstNumber).is_integer():
+    firstNumber = int(firstNumber)
+if float(secondNumber).is_integer():
+    secondNumber = int(secondNumber)
+sum = firstNumber + secondNumber
+sub = firstNumber - secondNumber
+mult = firstNumber * secondNumber
+div = firstNumber / secondNumber
+pow = firstNumber ** secondNumber
+mod = firstNumber % secondNumber
+idiv = firstNumber // secondNumber
+print(f'''Sum: {sum},
+Subtraction (first - second): {sub},
+Multiplication: {mult},
+Division (first / second): {div},
+Power (first ** second): {pow},
+Division by mod (first mod second): {mod},
+Int division (first div second): {idiv} ''')
