5e8f08dd997c0ea982cdaeb9e8c37fbb90684239
V1rru <97950906+V1rru@users.noreply.github.com>
2022-12-06 17:14:41 +0300

    Update Ex4.py

--- Lesson 3/Ex4.py
+++ Lesson 3/Ex4.py
@@ -12,6 +12,25 @@ while True:
         a = float(input("Enter a = "))
         b = float(input("Enter b = "))
         c = float(input("Enter c = "))
+
+        # Calculating discriminant
+        D = (b ** 2) - (4 * a * c)
+        print("Discriminant =", D)
+
+        # Calculating roots
+        if D > 0:
+            x1 = (-b + math.sqrt(D)) / (2 * a)
+            x2 = (-b - math.sqrt(D)) / (2 * a)
+            print(f'''x1 = {x1}
+            x2 = {x2}''')
+        elif D == 0:
+            x = -b / (2 * a)
+            print(f'''x = {x}''')
+        elif D < 0:
+            x1 = (-b + cmath.sqrt(D)) / (2 * a)
+            x2 = (-b - cmath.sqrt(D)) / (2 * a)
+            print(f'''x1 = {x1}''')
+            print(f'''x2 = {x2}''')
         break
     elif choice == "complex":
         # Getting coefficients
@@ -27,25 +46,19 @@ while True:
         b = complex(bReal, bImag)
         c = complex(cReal, cImag)
 
+
+            # Calculating discriminant
+        D = (b ** 2) - (4 * a * c)
+        print("Discriminant =", D)
+
+        # Calculating roots
+        x1 = (-b + cmath.sqrt(D)) / (2 * a)
+        x2 = (-b - cmath.sqrt(D)) / (2 * a)
+        print(f'''x1 = {x1}''')
+        print(f'''x2 = {x2}''')
+
         break
     else: 
         print("Wrong word. Try again")
 
-# Calculating discriminant
-D = (b ** 2) - (4 * a * c)
-print("Discriminant =", D)
 
-# Calculating roots
-if D > 0:
-    x1 = (-b + math.sqrt(D)) / (2 * a)
-    x2 = (-b - math.sqrt(D)) / (2 * a)
-    print(f'''x1 = {x1}
-    x2 = {x2}''')
-elif D == 0:
-    x = -b / (2 * a)
-    print(f'''x = {x}''')
-elif D < 0:
-    x1 = (-b + cmath.sqrt(D)) / (2 * a)
-    x2 = (-b - cmath.sqrt(D)) / (2 * a)
-    print(f'''x1 = {x1}
-    x2 = {x2}''')
