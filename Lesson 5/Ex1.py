ada9ec72630a9eb23d6f3b7f02ddd5f94d6b4931
V1rru <97950906+V1rru@users.noreply.github.com>
2022-12-13 16:27:48 +0300

    .

--- Lesson 5/Ex1.py
+++ Lesson 5/Ex1.py
@@ -5,12 +5,31 @@
 # Не должен состоять только из цифр;
 # Не должен содержать слово “password” в любом регистре.
 
-password = input("Enter you password: ").split()
+password = input("Enter you password: ")
 
-# def passwordCheck(password): 
+def passwordCheck(passwrd): 
 
-#     if (len(password) > 5):
+    checkLength = 0         # The flag is raised when the length is more than 5 symbols
+    checkDigit = 0          # The flag is raised when there is even one digit
+    checkNotAllDigit = 0    # The flag is raised when there are other symbols (not only digits)
+    checkPasswd = 0         # The flag is raised when the password is not "password"
 
+    if len(passwrd) > 5:
+        checkLength = 1
 
-#     return True
+    for i in range(len(passwrd)):
+        if passwrd[i].isdigit():
+            checkDigit = 1
 
+    if passwrd.isnumeric() == False:
+        checkNotAllDigit = 1
+
+    if passwrd.lower() != "password":
+        checkPasswd = 1
+
+    if checkLength == 1 and checkDigit == 1 and checkNotAllDigit == 1 and checkPasswd == 1:
+        return True
+    else:
+        return False
+
+print(passwordCheck(password))
