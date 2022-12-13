992527bbea8da6cb815d6c1c653fd2f63ebf3244
V1rru <97950906+V1rru@users.noreply.github.com>
2022-12-13 19:47:59 +0300

    Create Ex2.py

---
+++ Lesson 5/Ex2.py
@@ -0,0 +1,18 @@
+# Написать функцию, принимающую сколько угодно параметров (в том числе и 0) и возвращающую их сумму.
+
+def sum(*args):
+
+    list = []
+
+    for arg in args:
+        list.append(arg)
+
+    for i in range(1, len(list)):
+        list[0] += list[i]
+    
+    print(list[0])
+
+sum(10, 20, 30, 40, 50, 60)
+sum("Hello", ", ", "world!")
+sum([1, 2, 3], [4, 5, 6])
+sum((1, 2, 3), (4, 5, 6))
