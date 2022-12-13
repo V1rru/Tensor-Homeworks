87d9113c955cb9e0ddbb0c0621d4ce4903fb9c57
V1rru <97950906+V1rru@users.noreply.github.com>
2022-12-13 20:07:24 +0300

    Create Ex3.py

---
+++ Lesson 5/Ex3.py
@@ -0,0 +1,13 @@
+# Написать функцию, которая возвращает заданное число Фибоначчи (рекурсия)
+
+def fib(amount):
+    if (amount == 1 or amount == 2):
+        return 1
+    return fib(amount - 1) + fib(amount - 2)
+
+number = input("Enter an integer number: ")
+if number.isnumeric() == True:
+    number = int(number)
+    print("Your current Fibonacci number is:", fib(number))
+else:
+    print("You entered the wrong value")
